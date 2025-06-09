3ab8c0a8fcfc3c4955fa9d0da9140de7c090d892
woohm402 <woohm404@gmail.com>
2025-06-09 19:22:34 +0900

    unity 에서 http call 나가는 것 구현

--- SchoolRush/Assets/Scripts/PlayerDataSaveLoad.cs
+++ SchoolRush/Assets/Scripts/PlayerDataSaveLoad.cs
@@ -1,10 +1,27 @@
 using UnityEngine;
-using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using System.Text;
+using System;
 
 public static class PlayerDataSaveLoad {
     public static void SaveData(PlayerData playerData) {
-        string json = JsonUtility.ToJson(playerData);
-        string timestamp = ((int)System.DateTimeOffset.UtcNow.ToUnixTimeSeconds()).ToString();
-        File.WriteAllText(Path.Combine(Application.persistentDataPath, $"playerDatas-{timestamp}.json"), json);
+        try {
+            string json = JsonUtility.ToJson(playerData);
+            Debug.Log(json);
+            string timestamp = ((int)System.DateTimeOffset.UtcNow.ToUnixTimeSeconds()).ToString();
+
+            UnityWebRequest request = new UnityWebRequest("http://localhost:4000", "POST");
+
+            byte[] bodyRaw = Encoding.UTF8.GetBytes(json);
+            request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+            request.downloadHandler = new DownloadHandlerBuffer();
+            request.SendWebRequest();
+            Debug.Log("Request sent");
+        } catch (Exception e) {
+            Debug.Log("A");
+            Debug.LogError("Failed to save data: " + e.Message);
+        }
     }
 }
