2135f8858da01ac9d519fc54b57ad873f4ee55bc
woohm402 <woohm404@gmail.com>
2025-06-14 11:34:12 +0900

    데이터 10초마다 업데이트하는걸로 변경

--- SchoolRush/Assets/Scripts/PlayerDataSaveLoad.cs
+++ SchoolRush/Assets/Scripts/PlayerDataSaveLoad.cs
@@ -9,10 +9,9 @@ public static class PlayerDataSaveLoad {
     public static void SaveData(PlayerData playerData) {
         try {
             string json = JsonUtility.ToJson(playerData);
-            Debug.Log(json);
             string timestamp = ((int)System.DateTimeOffset.UtcNow.ToUnixTimeSeconds()).ToString();
 
-            UnityWebRequest request = new UnityWebRequest("http://localhost:4000", "POST");
+            UnityWebRequest request = new UnityWebRequest("https://schoolrush.vercel.app/api/player-data", "POST");
 
             byte[] bodyRaw = Encoding.UTF8.GetBytes(json);
             request.uploadHandler = new UploadHandlerRaw(bodyRaw);
@@ -20,7 +19,6 @@ public static class PlayerDataSaveLoad {
             request.SendWebRequest();
             Debug.Log("Request sent");
         } catch (Exception e) {
-            Debug.Log("A");
             Debug.LogError("Failed to save data: " + e.Message);
         }
     }
