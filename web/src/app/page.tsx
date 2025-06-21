a9cc539780e67ff68a3a17dcbbd716d6c712f4ec
woohm402 <woohm404@gmail.com>
2025-06-21 17:57:17 +0900

    이동 경로 정확하게 뜨도록 구현

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,10 +1,10 @@
-import type { PlayerData } from '@/entities/player-data';
-import { UPGRADE_IDS } from '@/entities/upgrade';
-import { ensure } from '@/utils/ensure';
-import { MongoClient, type ObjectId, ServerApiVersion } from 'mongodb';
-import { Item } from './_components/Item';
+import type { PlayerData } from "@/entities/player-data";
+import { UPGRADE_IDS } from "@/entities/upgrade";
+import { ensure } from "@/utils/ensure";
+import { MongoClient, type ObjectId, ServerApiVersion } from "mongodb";
+import { Item } from "./_components/Item";
 
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 
 export default async function Home() {
   const mongodbUrl = `mongodb+srv://woohm404:${process.env.MONGODB_PASSWORD}@cluster0.qtwt5z8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -13,8 +13,8 @@ export default async function Home() {
   });
 
   const data = (await client
-    .db('player-data')
-    .collection('players')
+    .db("player-data")
+    .collection("players")
     .find()
     .sort({ totalTime: 1 })
     .limit(10)
@@ -27,12 +27,14 @@ export default async function Home() {
           <Item
             log={item.logs.map((l) => ({
               time: l.time,
-              position: { x: l.x, y: l.y },
+              position: { x: l.x, y: l.z },
             }))}
             nickname={item.nickname}
             rank={i + 1}
             totalTime={item.totalTime}
-            upgradeIds={item.augmentIds.map((id) => ensure(UPGRADE_IDS.find((i) => i === id)))}
+            upgradeIds={item.augmentIds.map((id) =>
+              ensure(UPGRADE_IDS.find((i) => i === id)),
+            )}
           />
         </li>
       ))}
