338ad3d55df5f1edbba85f75f8180c824d0c0e7f
Haolin Zhang <yoctoyotta9@gmail.com>
2025-06-21 16:46:57 +0900

    Minimap distance Added

--- SchoolRush/Assets/Scripts/Map/MMIndicator.cs
+++ SchoolRush/Assets/Scripts/Map/MMIndicator.cs
@@ -4,20 +4,18 @@ using UnityEngine;
 
 public class MMIndicator : MonoBehaviour
 {
-    private float length;
+    public TMPro.TextMeshProUGUI distUI;
     public GameObject player;
+    private float length = 20f;
     private GameObject checkpoint;
 
-    private float amp = 2f;
-    private float freq = 2f;
-
-    private void Start() {
-        length = 20f;
-    }
+    private const float amp = 2f;
+    private const float freq = 2f;
 
     private void Update()
     {
-        Vector3 dir = Vector3.Normalize(checkpoint.transform.position - player.transform.position);
+        Vector3 vector = checkpoint.transform.position - player.transform.position;
+        Vector3 dir = Vector3.Normalize(vector);
         transform.position = player.transform.position + (length + amp * Mathf.Sin(Time.time * freq)) * dir;
 
         Quaternion rotation = Quaternion.LookRotation(dir, transform.up);
@@ -25,6 +23,8 @@ public class MMIndicator : MonoBehaviour
 
         Vector3 pos = transform.position;
         transform.position = new(pos.x, player.transform.position.y + 30, pos.z);
+
+        distUI.text = $"Target: {vector.magnitude:F0}m";
     }
 
     public void SetCheckpoint(GameObject cp){
